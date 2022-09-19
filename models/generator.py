ae6593d4d56ffd5cc999e2dd815efc2a24973608
DooDooKim <arkimjh@kaist.ac.kr>
2022-09-19 20:04:30 +0900

    v0.1

---
+++ models/generator.py
@@ -0,0 +1,161 @@
+import torch
+import torch.nn as nn
+from torch.nn import init
+import functools
+from torch.autograd import Variable
+from torch.optim import lr_scheduler
+import numpy as np
+import math
+
+def weights_init(m, act_type='relu'):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        if act_type == 'selu':
+            n = float(m.in_channels * m.kernel_size[0] * m.kernel_size[1])
+            m.weight.data.normal_(0.0, 1.0 / math.sqrt(n))
+        else:
+            m.weight.data.normal_(0.0, 0.02)
+        if hasattr(m.bias, 'data'):
+            m.bias.data.fill_(0)
+    elif classname.find('BatchNorm2d') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
+def get_scheduler(optimizer, args):
+    if args.lr_policy == 'lambda':
+        def lambda_rule(epoch):
+            lr_l = ((0.5 ** int(epoch >= 2)) *
+                    (0.5 ** int(epoch >= 5)) *
+                    (0.5 ** int(epoch >= 8)))
+            return lr_l
+
+        scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda_rule)
+    elif args.lr_policy == 'step':
+        scheduler = lr_scheduler.StepLR(
+            optimizer, step_size=args.lr_decay_iters, gamma=0.1
+        )
+    elif args.lr_policy == 'plateau':
+        scheduler = lr_scheduler.ReduceLROnPlateau(
+            optimizer, mode='min', factor=0.2, threshold=0.01, patience=5
+        )
+    else:
+        return NotImplementedError('learning rate policy [%s] is not implemented', args.lr_policy)
+    return scheduler
+
+class ResnetGenerator(nn.Module):
+    def __init__(self, input_nc, output_nc, ngf, norm_type, act_type='selu', use_dropout=False, n_blocks=6,
+                 padding_type='reflect', mean=None, std=None):
+        assert (n_blocks >= 0)
+        super(ResnetGenerator, self).__init__()
+
+        self.name = 'resnet'
+        self.input_nc = input_nc
+        self.output_nc = output_nc
+        self.ngf = ngf
+        self.mean = mean.view(1, -1, 1, 1)
+        self.std = std.view(1, -1, 1, 1)
+
+        use_bias = norm_type == 'instance'
+
+        if norm_type == 'batch':
+            norm_layer = nn.BatchNorm2d
+        elif norm_type == 'instance':
+            norm_layer = nn.InstanceNorm2d
+
+        if act_type == 'selu':
+            self.act = nn.SELU(True)
+        else:
+            self.act = nn.ReLU(True)
+
+        model0 = [nn.ReflectionPad2d(3), nn.Conv2d(input_nc, ngf, kernel_size=7, padding=0, bias=use_bias),
+                  norm_layer(ngf), self.act]
+
+        n_downsampling = 2
+        for i in range(n_downsampling):
+            mult = 2 ** i
+            model0 += [nn.Conv2d(ngf * mult, ngf * mult * 2, kernel_size=3, stride=2, padding=1, bias=use_bias),
+                       norm_layer(ngf * mult * 2), self.act]
+
+        mult = 2 ** n_downsampling
+        for i in range(n_blocks):
+            model0 += [
+                ResnetBlock(ngf * mult, padding_type=padding_type, norm_layer=norm_layer, use_dropout=use_dropout,
+                            use_bias=use_bias)]
+
+        for i in range(n_downsampling):
+            mult = 2 ** (n_downsampling - i)
+            model0 += [nn.ConvTranspose2d(ngf * mult, int(ngf * mult / 2),
+                                          kernel_size=3, stride=2,
+                                          padding=1, output_padding=1,
+                                          bias=use_bias),
+                       norm_layer(int(ngf * mult / 2)),
+                       self.act]
+        model0 += [nn.ReflectionPad2d(3)]
+        model0 += [nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0)]
+        model0 += [nn.Tanh()]
+
+        self.model0 = nn.Sequential(*model0)
+
+    def forward(self, x):
+        x = (x - self.mean) / self.std
+        x = self.model0(x)
+
+        return x
+
+# Define a resnet block
+class ResnetBlock(nn.Module):
+    def __init__(self, dim, padding_type, norm_layer, use_dropout, use_bias):
+        super(ResnetBlock, self).__init__()
+        self.conv_block = self.build_conv_block(dim, padding_type, norm_layer, use_dropout, use_bias)
+
+    def build_conv_block(self, dim, padding_type, norm_layer, use_dropout, use_bias):
+        conv_block = []
+        p = 0
+        if padding_type == 'reflect':
+            conv_block += [nn.ReflectionPad2d(1)]
+        elif padding_type == 'replicate':
+            conv_block += [nn.ReplicationPad2d(1)]
+        elif padding_type == 'zero':
+            p = 1
+        else:
+            raise NotImplementedError('padding [%s] is not implemented' % padding_type)
+
+        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=p, bias=use_bias),
+                       norm_layer(dim),
+                       nn.ReLU(True)]
+        if use_dropout:
+            conv_block += [nn.Dropout(0.5)]
+
+        p = 0
+        if padding_type == 'reflect':
+            conv_block += [nn.ReflectionPad2d(1)]
+        elif padding_type == 'replicate':
+            conv_block += [nn.ReplicationPad2d(1)]
+        elif padding_type == 'zero':
+            p = 1
+        else:
+            raise NotImplementedError('padding [%s] is not implemented' % padding_type)
+        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=p, bias=use_bias),
+                       norm_layer(dim)]
+
+        return nn.Sequential(*conv_block)
+
+    def forward(self, x):
+        out = x + self.conv_block(x)
+        return out
+
+def daml_gen(dataset='imagenet', mean=None, std=None):
+
+    if dataset == 'cifar10' or dataset == 'svhn':
+        ngf = 64
+    elif dataset == 'cifar100':
+        ngf = 64
+    elif dataset == 'tiny':
+        ngf = 64
+    elif dataset == 'imagenet':
+        ngf = 64
+    else:
+        raise NotImplementedError
+
+    return ResnetGenerator(3, 3, ngf, norm_type='batch', act_type='relu', mean=mean, std=std)
+
