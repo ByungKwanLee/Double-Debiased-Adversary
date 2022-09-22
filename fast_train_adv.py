e43ee7da2b5c7b8401892ea802dc771b84cbb058
DooDooKim <arkimjh@kaist.ac.kr>
2022-09-22 14:14:17 +0900

    v0.1

--- fast_train_adv.py
+++ fast_train_adv.py
@@ -29,10 +29,10 @@ parser = argparse.ArgumentParser()
 
 # model parameter
 parser.add_argument('--NAME', default='ADV', type=str)
-parser.add_argument('--dataset', default='tiny', type=str)
-parser.add_argument('--network', default='wide', type=str)
-parser.add_argument('--depth', default=28, type=int) # 12 for vit
-parser.add_argument('--gpu', default='0,1,2,3', type=str)
+parser.add_argument('--dataset', default='svhn', type=str)
+parser.add_argument('--network', default='vit', type=str)
+parser.add_argument('--depth', default=12, type=int) # 12 for vit
+parser.add_argument('--gpu', default='0,1,2,3,4', type=str)
 parser.add_argument('--port', default="12355", type=str)
 
 # transformer parameter
@@ -44,8 +44,8 @@ parser.add_argument("--num_steps", default=10000, type=int)
 parser.add_argument('--pretrain', default=False, type=bool)
 
 # learning parameter
-parser.add_argument('--epochs', default=30, type=int)
-parser.add_argument('--learning_rate', default=0.5, type=float) #3e-2 for ViT
+parser.add_argument('--epochs', default=300, type=int)
+parser.add_argument('--learning_rate', default=3e-2, type=float) #3e-2 for ViT
 parser.add_argument('--weight_decay', default=5e-4, type=float)
 parser.add_argument('--batch_size', default=128, type=float)
 parser.add_argument('--test_batch_size', default=64, type=float)
