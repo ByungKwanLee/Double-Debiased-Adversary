df5066577f12cb4bd58fe753b173d5e3458acd1a
DooDooKim <arkimjh@kaist.ac.kr>
2022-10-26 17:45:13 +0900

    v0.1

--- fast_train_daml.py
+++ fast_train_daml.py
@@ -34,7 +34,7 @@ parser.add_argument('--NAME', default='DAML', type=str)
 parser.add_argument('--dataset', default='cifar10', type=str)
 parser.add_argument('--network', default='vgg', type=str)
 parser.add_argument('--depth', default=16, type=int) # 12 for vit
-parser.add_argument('--gpu', default='0,1,2,3', type=str)
+parser.add_argument('--gpu', default='0,1,2,3,4', type=str)
 parser.add_argument('--port', default="12356", type=str)
 
 # transformer parameter
@@ -263,12 +263,12 @@ def main_worker(rank, ngpus_per_node=ngpus_per_node):
         checkpoint = torch.load(pretrain_ckpt_name, map_location=torch.device(torch.cuda.current_device()))
     else:
         # adv
-        pretrain_ckpt_name = f'checkpoint/adv/{args.dataset}/{args.dataset}_adv_{args.network}{args.depth}_best.t7'
-        checkpoint = torch.load(pretrain_ckpt_name, map_location=torch.device(torch.cuda.current_device()))
+        # pretrain_ckpt_name = f'checkpoint/adv/{args.dataset}/{args.dataset}_adv_{args.network}{args.depth}_best.t7'
+        # checkpoint = torch.load(pretrain_ckpt_name, map_location=torch.device(torch.cuda.current_device()))
 
-        # generator
-        pretrain_generator_ckpt_name = f'checkpoint/generator/{args.dataset}/{args.dataset}_generator_{args.network}{args.depth}_best.t7'
-        checkpoint_generator = torch.load(pretrain_generator_ckpt_name, map_location=torch.device(torch.cuda.current_device()))
+        # standard
+        pretrain_ckpt_name = f'checkpoint/standard/{args.dataset}/{args.dataset}_{args.network}{args.depth}_best.t7'
+        checkpoint = torch.load(pretrain_ckpt_name, map_location=torch.device(torch.cuda.current_device()))
 
     # network f
     # init model and Distributed Data Parallel
